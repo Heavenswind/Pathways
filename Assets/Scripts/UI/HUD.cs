7a2029f6a62d8bc6efb4909352b6e298d69581aa
Samuel Proulx <proulxsamuel@gmail.com>
2019-03-26 22:03:44 -0400

    Implement team scores

--- Assets/Scripts/UI/HUD.cs
+++ Assets/Scripts/UI/HUD.cs
@@ -8,6 +8,10 @@ public class HUD : MonoBehaviour
     public static HUD instance = null;
 
     [SerializeField] private GameObject healthBarPrefab = null;
+    [SerializeField] private Slider blueTeamProgress = null;
+    [SerializeField] private Text blueTeamProgressLabel = null;
+    [SerializeField] private Slider redTeamProgress = null;
+    [SerializeField] private Text redTeamProgressLabel = null;
 
     void Awake()
     {
@@ -24,5 +28,13 @@ public class HUD : MonoBehaviour
         healthBar.transform.SetParent(transform);
         healthBar.ConnectTo(unit);
         return healthBar;
+    }
+
+    public void SetProgress(float blue, float red)
+    {
+        blueTeamProgress.value = blue;
+        blueTeamProgressLabel.text = string.Format("Blue team progress ({0}%)", Mathf.Round(blue * 100));
+        redTeamProgress.value = red;
+        redTeamProgressLabel.text = string.Format("Red team progress ({0}%)", Mathf.Round(red * 100));
     }
 }
